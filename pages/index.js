35344d8b653b3eef9be95af1fc68661b8401c3a4
hspadim <henrique@spadim.com.br>
2021-01-30 11:13:04 -0300

    Add "estrutura base"

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import styled from 'styled-components'
 import db from '../db.json';
+import { Widget } from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
 
 // const Title = styled.h1`
 //   font-size: 50px;
 //   color: ${({ theme }) => theme.colors.primary};
 // `;
 
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -24,13 +28,34 @@ export const QuizContainer = styled.div`
   }
 `;
 
+
+
 export default function Home() {
   // return <Title>My page</Title>
   return (
-    <BackgroundImage>
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        asdasdasd
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+
+          <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
       </QuizContainer>
-    </BackgroundImage>
+      <GitHubCorner projectUrl="https://github.com/midapsh"/>
+    </QuizBackground>
   )
 }
